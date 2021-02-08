dcb6a3d7f79229de7c127b83d6d2f8b08429daed
haitham-bjanthalah <haitham.bjanthalah@viaboxx.de>
2021-02-08 11:17:24 +0100

    multiple sources for a java config - using PropertySource and PropertySources annotations

---
+++ src/main/java/guru/springframework/sfgdi/examplebeans/FakeJmsBroker.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgdi.examplebeans;
+
+import lombok.Data;
+
+@Data
+public class FakeJmsBroker {
+    private String user;
+    private String password;
+    private String url;
+
+}
