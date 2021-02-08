1f6b500680a10a244497b1484594ebf75e5ab7bb
haitham-bjanthalah <haitham.bjanthalah@viaboxx.de>
2021-02-08 10:50:30 +0100

    simple java config example

---
+++ src/main/java/guru/springframework/sfgdi/examplebeans/FakeDataSource.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgdi.examplebeans;
+
+import lombok.Data;
+
+@Data
+public class FakeDataSource {
+    private String user;
+    private String password;
+    private String url;
+
+}
